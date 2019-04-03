8a148b48b603b99842e03be4cfddfc72649f6750
Tak H Kim <takhyun818@gmail.com>
2019-04-03 19:47:48 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+         item_name = req.path.split("/items/").last
+         item = @@items.find{|item| item.name == item_name}
+         if !item
+           resp.status = 400
+           resp.write "Item not found"
+         else
+            resp.write item.price
+         end
+    else
+      resp.status=404
+      resp.write "Route not found"
+    end
+     resp.finish
+  end
+end
